83a615742c1f25ba8177d64bbafab3372dd0c9f0
stanus <kucharik.s@gmail.com>
2020-01-12 15:01:36 +0100

    init extension

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,7 +18,8 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  },
+  { path: "*", redirect: "/" }
 ];
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="log">log</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { Component, Vue } from "vue-property-decorator";
 
-export default {
-  name: "home",
+@Component({
   components: {
     HelloWorld
   }
-};
+})
+export default class Home extends Vue {
+  log() {
+    console.log("gule");
+  }
+}
 </script>
