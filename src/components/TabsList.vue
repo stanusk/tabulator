20cb68760f7be643fe83bf1ed7cba3013f50b97b
stanus <kucharik.s@gmail.com>
2020-01-16 20:02:18 +0100

    add logic to TabsList

--- src/components/TabsList.vue
+++ src/components/TabsList.vue
@@ -2,15 +2,38 @@
     <b-list-group>
         <b-list-group-item
             button
-            v-for="(title, i) of tabTitles"
-            v-bind:key="i"
+            v-for="tab of tabs"
+            v-bind:key="tab.id"
+            :active="tab.active"
+            @click="highlight(tab.id)"
             class="d-flex justify-content-between align-items-center"
         >
             <span class="tab-title-text">
-                {{ title }}
+                {{ tab.title }}
             </span>
-            <b-form-checkbox> </b-form-checkbox>
-            <b-icon icon="x-square-fill" variant="danger"> </b-icon>
+
+            <b-icon
+                v-if="isSelected(tab)"
+                icon="star-fill"
+                @click.stop="toggleSelected(tab)"
+                class="h3"
+            >
+            </b-icon>
+            <b-icon
+                v-else
+                icon="star"
+                @click.stop="toggleSelected(tab)"
+                class="h3"
+            >
+            </b-icon>
+
+            <b-icon
+                icon="x-square-fill"
+                variant="dark"
+                @click="closeTab(tab.id)"
+                class="h3"
+            >
+            </b-icon>
         </b-list-group-item>
     </b-list-group>
 </template>
@@ -21,17 +44,44 @@ import Tab = browser.tabs.Tab;
 
 @Component
 export default class TabsList extends Vue {
+    tabs: Tab[] = [];
+    selectedTabs: Tab[] = [];
+
     mounted() {
         browser.tabs.query({}).then((tabs: Tab[]) => {
-            this.tabTitles = tabs.map(t => t.title || 'UNKNOWN');
+            this.tabs = tabs;
+        });
+    }
+
+    highlight(tabId: number) {
+        browser.tabs.update(tabId, { active: true });
+    }
+
+    isSelected(tab: Tab) {
+        return this.selectedTabs.includes(tab);
+    }
+
+    toggleSelected(tab: Tab) {
+        const addTab = (addedTab: Tab) => {
+            return [...this.selectedTabs, addedTab];
+        };
+
+        const removeTab = (removedTab: Tab) => {
+            return this.selectedTabs.filter(t => t.id !== removedTab.id);
+        };
+
+        this.selectedTabs = this.isSelected(tab) ? removeTab(tab) : addTab(tab);
+    }
+
+    closeTab(tabId: number) {
+        browser.tabs.remove(tabId).then(_ => {
+            this.tabs = this.tabs.filter(tab => tab.id !== tabId);
         });
     }
 
     // get defaultText() {
     //   return browser.i18n.getMessage("extName");
     // }
-
-    tabTitles: string[] = [];
 }
 </script>
 
