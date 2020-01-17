a96f2bacbc6dd543078d7c3975c2f81af080bbe7
stanus <kucharik.s@gmail.com>
2020-01-17 12:15:02 +0100

    implement basic revive tabs functionality in Projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,19 @@
 <template>
-    <div class="projects"></div>
+    <div class="projects">
+        <b-list-group>
+            <b-list-group-item
+                v-for="project in projects"
+                v-bind:key="project.name"
+                class="d-flex justify-content-between align-items-center"
+            >
+                <span>{{ project.name }}</span>
+                <b-button variant="outline-primary">
+                    <b-icon icon="box-arrow-up-right" @click="revive(project)">
+                    </b-icon>
+                </b-button>
+            </b-list-group-item>
+        </b-list-group>
+    </div>
 </template>
 
 <script lang="ts">
@@ -13,6 +27,25 @@ export default class Projects extends Vue {
         browser.storage.sync.get('projects').then(result => {
             this.projects = JSON.parse(result.projects);
         });
+    }
+
+    revive(project: Project) {
+        const urls: string[] = project.tabs.map(tab => {
+            const url = tab.url || '';
+            const urlTargetIndex = url.indexOf('http');
+
+            return url.substring(urlTargetIndex);
+        });
+        browser.windows.create({ url: urls });
+
+        this.projects = this.projects.filter(p => p.name !== project.name);
+        this.uploadStoredProjects();
+    }
+
+    // todo: unify with uploadStoredProjects in Home.vue
+    uploadStoredProjects() {
+        const projectsString = JSON.stringify(this.projects);
+        browser.storage.sync.set({ projects: projectsString });
     }
 }
 </script>
