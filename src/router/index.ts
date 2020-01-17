58b5564bd7a77e1c8a4ad80584e1412c4c151ac7
stanus <kucharik.s@gmail.com>
2020-01-17 08:43:22 +0100

    add Projects view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,31 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  { path: "*", redirect: "/" }
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+    },
+    {
+        path: '/projects',
+        name: 'projects',
+        // route level code-splitting
+        // this generates a separate chunk (projects.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "projects" */ '../views/Projects.vue'),
+    },
+    { path: '*', redirect: '/' },
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes,
 });
 
 export default router;
---
+++ src/views/Projects.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="projects"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Project } from '@/typings';
+
+@Component
+export default class Projects extends Vue {
+    projects: Project[] = [];
+    created(): void {
+        browser.storage.sync.get('projects').then(result => {
+            this.projects = JSON.parse(result.projects);
+        });
+    }
+}
+</script>
