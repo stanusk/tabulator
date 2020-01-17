1b272185c86e27af3e6901048916a9d9fc441d9c
stanus <kucharik.s@gmail.com>
2020-01-17 13:27:22 +0100

    add tabs list to Projects and fix design

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,18 +1,31 @@
 <template>
     <div class="projects">
-        <b-list-group>
-            <b-list-group-item
-                v-for="project in projects"
-                v-bind:key="project.name"
-                class="d-flex justify-content-between align-items-center"
+        <b-card
+            v-for="project in projects"
+            v-bind:key="project.name"
+            body-class="m-0 p-0"
+            v-b-toggle.collapse-1-inner
+        >
+            <b-container
+                class="project-header d-flex justify-content-between align-items-center"
             >
-                <span>{{ project.name }}</span>
-                <b-button variant="outline-primary">
+                <p class="project-name card-text">{{ project.name }}</p>
+                <b-button variant="outline-primary" size="sm">
                     <b-icon icon="box-arrow-up-right" @click="revive(project)">
                     </b-icon>
                 </b-button>
-            </b-list-group-item>
-        </b-list-group>
+            </b-container>
+            <b-collapse id="collapse-1-inner" class="tabs">
+                <b-list-group flush>
+                    <b-list-group-item
+                        class="m-1 p-1"
+                        v-for="tab in project.tabs"
+                        v-bind:key="tab.id"
+                        >{{ tab.title }}</b-list-group-item
+                    >
+                </b-list-group>
+            </b-collapse>
+        </b-card>
     </div>
 </template>
 
@@ -49,3 +62,21 @@ export default class Projects extends Vue {
     }
 }
 </script>
+
+<style scoped lang="scss">
+.projects {
+    padding: 5px;
+    .project-header {
+        padding: 5px;
+        background-color: azure;
+        .project-name {
+            margin: auto 0;
+            font-weight: bold;
+        }
+    }
+    .tabs {
+        border-top: 1px solid lightgrey;
+        font-size: 0.8rem;
+    }
+}
+</style>
