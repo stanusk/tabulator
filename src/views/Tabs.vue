7e9d6de32602d95779d52b9289f5423eb49b363d
stanus <kucharik.s@gmail.com>
2020-02-03 13:02:42 +0100

    hide helpers

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <DevHelpers></DevHelpers>
+        <!--<DevHelpers></DevHelpers>-->
         <CreateProject
             :disabled="selectedTabs.length === 0"
             :projectName="newProjectName"
