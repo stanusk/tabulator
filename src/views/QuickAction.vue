315d42dba6d55ec87372bcca1cf36b142f00a6d5
stanus <kucharik.s@gmail.com>
2020-02-28 17:32:35 +0100

    use arrows up & down to change selected

--- src/views/QuickAction.vue
+++ src/views/QuickAction.vue
@@ -6,17 +6,26 @@
             v-model="searchInput"
             debounce="500"
             :autofocus="true"
+            @keydown="onKeydown($event)"
         >
         </b-form-input>
 
-        <b-card v-for="bWindow in filteredWindows" v-bind:key="bWindow.id">
+        <b-card
+            v-for="(bWindow, windowIndex) in filteredWindows"
+            v-bind:key="bWindow.id"
+        >
             <b-list-group flush>
                 <b-list-group-item
                     button
-                    v-for="tab of bWindow.tabs"
+                    v-for="(tab, tabIndex) of bWindow.tabs"
                     v-bind:key="tab.id"
                     @click="activateTab(tab.id, bWindow.id)"
                     class="tab p-2"
+                    :class="{
+                        active:
+                            tabIndex === selectedTabIndex &&
+                            windowIndex === selectedWindowIndex,
+                    }"
                 >
                     {{ tab.title }}
                 </b-list-group-item>
@@ -43,11 +52,16 @@ export default class QuickAction extends Vue {
     bWindows!: WindowClean[];
 
     searchInput: string = '';
+    selectedTabIndex: number = 0;
+    selectedWindowIndex: number = 0;
 
     get filteredWindows(): WindowClean[] {
         if (this.searchInput === '') {
             return [];
         }
+
+        this.selectedTabIndex = 0;
+        this.selectedWindowIndex = 0;
 
         return this.bWindows.reduce((filteredWindows, currentWindow) => {
             const windowWithFilteredTabs = this.filterWindow(
@@ -59,6 +73,65 @@ export default class QuickAction extends Vue {
                 ? [...filteredWindows, windowWithFilteredTabs]
                 : filteredWindows;
         }, [] as WindowClean[]);
+    }
+
+    onKeydown(event: KeyboardEvent) {
+        switch (event.key) {
+            case 'Enter': {
+                const selectedWindow = this.filteredWindows[
+                    this.selectedWindowIndex
+                ];
+                const selectedTab = selectedWindow.tabs[this.selectedTabIndex];
+                this.activateTab(selectedTab.id, selectedWindow.id);
+                break;
+            }
+            case 'ArrowDown': {
+                event.stopPropagation();
+                event.preventDefault();
+                const selectedWindow = this.filteredWindows[
+                    this.selectedWindowIndex
+                ];
+                if (selectedWindow.tabs.length > this.selectedTabIndex + 1) {
+                    this.selectNextTab();
+                } else if (
+                    this.filteredWindows.length >
+                    this.selectedWindowIndex + 1
+                ) {
+                    this.selectNextWindow();
+                }
+                break;
+            }
+            case 'ArrowUp': {
+                event.stopPropagation();
+                event.preventDefault();
+                if (this.selectedTabIndex > 0) {
+                    this.selectPreviousTab();
+                } else if (this.selectedWindowIndex > 0) {
+                    this.selectPreviousWindow();
+                }
+                break;
+            }
+        }
+    }
+
+    private selectNextTab() {
+        this.selectedTabIndex++;
+    }
+
+    private selectPreviousTab() {
+        this.selectedTabIndex--;
+    }
+
+    private selectNextWindow() {
+        this.selectedWindowIndex++;
+        this.selectedTabIndex = 0;
+    }
+
+    private selectPreviousWindow() {
+        this.selectedWindowIndex--;
+        const selectedWindow = this.filteredWindows[this.selectedWindowIndex];
+
+        this.selectedTabIndex = selectedWindow.tabs.length - 1;
     }
 
     activateTab(tabId: number, windowId: number) {
