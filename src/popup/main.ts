3769031a32be9533dfd787966757a02502ce5e0f
stanus <kucharik.s@gmail.com>
2020-05-31 09:47:43 +0200

    replace bootstrap with quasar: delete bootstrap

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -2,8 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '../router';
 import store from '../store';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import './custom.scss';
 
 import { Quasar } from 'quasar';
 import '../styles/quasar.scss';
@@ -12,8 +10,6 @@ import '@quasar/extras/material-icons/material-icons.css';
 
 Vue.config.productionTip = false;
 
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
 Vue.use(Quasar);
 
 new Vue({
